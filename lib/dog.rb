d273d77e174f6c5dad017ed0d904a04e8a02186a
Jenny Chu <jchudesign0930@gmail.com>
2019-03-03 02:43:18 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,15 @@
+class Dog 
+  
+  def initialize (name,breed="Mutt")
+    @name = name
+    @breed = breed
+  end
+  
+  def breed
+    @breed
+  end
+  
+  def breed= (new_breed)
+    @breed
+  end
+end
